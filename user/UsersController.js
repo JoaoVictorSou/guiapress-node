0df8372a36fb705c73172a36f82b58e7ecdc10dd
João Victor <soulima.joao@gmail.com>
2021-11-09 15:13:37 -0300

    barrando cadastro de emails repetidos

--- user/UsersController.js
+++ user/UsersController.js
@@ -14,20 +14,33 @@ router.get('/admin/users/new', (req, res) => {
 router.post('/users/save', (req, res) => {
     const email = req.body.email || null
     const password = req.body.password || null
-    const salt = bcrypt.genSaltSync(10)
-    const hash = bcrypt.hashSync(password, salt)
-
+    
     User
-        .create({
-            email: email,
-            password: hash
+        .findOne({
+            where: {
+                email: email
+            }
         })
-        .then(() => {
-            res.redirect('/admin/users')
-        })
-        .catch((err) => {
-            console.log(`[ERR] user IS NOT saved: ${err}`)
-            res.redirect('/admin/users/new')
+        .then((user) => {
+            if (!user) {
+                const salt = bcrypt.genSaltSync(10)
+                const hash = bcrypt.hashSync(password, salt)
+
+                User
+                    .create({
+                        email: email,
+                        password: hash
+                    })
+                    .then(() => {
+                        res.redirect('/admin/users')
+                    })
+                    .catch((err) => {
+                        console.log(`[ERR] user IS NOT saved: ${err}`)
+                        res.redirect('/')
+                    })
+            } else {
+                res.redirect('/admin/users/new')
+            }
         })
 })
 
