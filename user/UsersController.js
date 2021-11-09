bcac0aae5ded3362d2e6373f9adb1e235cceb83d
João Victor <soulima.joao@gmail.com>
2021-11-09 15:05:48 -0300

    cadastro de usuários no banco de daddos

--- user/UsersController.js
+++ user/UsersController.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const User = require('./User')
+const bcrypt = require('bcryptjs')
 
 router.get('/admin/users', (req, res) => {
     res.send('hello, world')
@@ -13,8 +14,21 @@ router.get('/admin/users/new', (req, res) => {
 router.post('/users/save', (req, res) => {
     const email = req.body.email || null
     const password = req.body.password || null
+    const salt = bcrypt.genSaltSync(10)
+    const hash = bcrypt.hashSync(password, salt)
 
-    res.json({email, password})
+    User
+        .create({
+            email: email,
+            password: hash
+        })
+        .then(() => {
+            res.redirect('/admin/users')
+        })
+        .catch((err) => {
+            console.log(`[ERR] user IS NOT saved: ${err}`)
+            res.redirect('/admin/users/new')
+        })
 })
 
 module.exports = router
